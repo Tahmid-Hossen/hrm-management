4e9ca0bde3f5607604975e60d78ba72f6e6b60b3
Tahmid Hossen <tahmid.hossen@nexdecade.com>
2024-04-22 18:33:56 +0600

    text change on employee page

--- resources/views/layouts/modals.blade.php
+++ resources/views/layouts/modals.blade.php
@@ -392,11 +392,7 @@
                                     <div class="flex gap-x-2">
                                         <div>
                                             <button type="file" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                                            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-                                                <polyline points="17 8 12 3 7 8"/>
-                                                <line x1="12" x2="12" y1="3" y2="15"/>
-                                            </svg>
+                                            <i class="fa-solid fa-download"></i>
                                             <input type="file" name="profile_photo" id="file-input" class="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400
                                                 file:bg-gray-50 file:border-0
                                                 file:me-4

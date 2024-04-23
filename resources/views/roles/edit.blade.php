6e7f3ec61e8a9216f667008536cee0511a3bd121
mahmud886 <iqbal886mahmud@gmail.com>
2024-04-23 13:41:41 +0600

    permission page redesign

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -1,38 +1,61 @@
 @extends('layouts.app')
-@section('title', 'Users')
+@section('title', 'Edit Role')
+@section('pageTitle', 'Edit Role')
+
+@section('breadcumb')
+    <ol class="flex items-center mt-2 whitespace-nowrap mr-2">
+        <li class="inline-flex items-center">
+            <a class="flex items-center text-sm text-gray-500 hover:text-red-600 focus:outline-none focus:text-neutral-600 dark:focus:text-neutral-600"
+               href="#">
+                <svg class="flex-shrink-0 me-3 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                     stroke-linejoin="round">
+                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                </svg>
+                Home
+            </a>
+            <svg class="flex-shrink-0 mx-2 overflow-visible text-gray-400 size-4 dark:text-neutral-600"
+                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m9 18 6-6-6-6"></path>
+            </svg>
+        </li>
+
+        <li class="inline-flex items-center text-sm font-semibold text-red-600 truncate dark:text-red-600"
+            aria-current="page">
+            Edit Role
+        </li>
+    </ol>
+@endsection
+
 @section('content')
-<div class="midde_cont">
-    <div class="relative col-span-6 md:col-span-5 p-4 pb-0 pt-0">
-        <div class="main-container min-h-[calc(100vh_-_120px)]">
-            <h3 class="text-2xl py-3 font-bold">
-                <a href="#" class="text-primary">
-                    <i class="fa-solid fa-arrow-left"></i>
-                </a>
-                Edit Role
-            </h3>
+<div class="flex flex-col p-2 border rounded-lg dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700">
+    <div class="relative col-span-6 md:col-span-5 pb-0 pt-0">
+        <div class="">
             <div class="w-full mt-4">
-                <form action="https://inventory-stg.nexdecade.com/roles/update/2" method="POST" enctype="multipart/form-data">
+                <form action="#" method="POST" enctype="multipart/form-data">
                     <input type="hidden" name="_token" value="X18razynt1ty8pwXe5fvP42PIbhUTchP04hBHYdo" autocomplete="off">
-                    <div class="rounded-[5px] bg-[#FFFFFF] border border-gray-300 border-t-[3px] border-t-[#DC2626] p-5 pt-3 mb-8" style="box-shadow: 2px 4px 12px -2px rgba(0, 0, 0, 0.2)">
-                        <h3 class="text-xl  mt-8"><i class="fa-solid fa-user-shield text-primary"></i> Edit for: <span class="font-semibold">admin</span> </h3>
-                        <hr class="my-2 mb-4 border-gray-300">
+                    <div class="p-5 pt-3">
+                        <h3 class="text-xl text-gray-600 dark:text-neutral-200"><i class="fa-solid fa-user-shield text-primary"></i> Edit for: <span class="font-semibold text-red-600 dark:text-red-800">admin</span> </h3>
+                        <hr class="my-2 mb-4 border-neutral-200 dark:border-neutral-700">
                         <div class=" flex gap-6 pt-6">
                             <div class="">
                                 <div class="flex items-center mt-2">
-                                    <label for="checkbox" class="block text-gray-700 text-sm font-bold">Is Active?</label>
+                                    <label for="checkbox" class="block text-red-600 text-sm font-bold">Is Active?</label>
                                     <div class="form-control">
                                         <label class="cursor-pointer label">
-                                            <input type="checkbox" name="is_active" class="checkbox checkbox-info ml-2 " checked="">
+                                            <input type="checkbox" name="is_active" class="shrink-0 mt-0.5 border-gray-200 rounded text-red-600 focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-red-500 dark:checked:border-red-500 dark:focus:ring-offset-red-800 ml-2 " checked="">
                                         </label>
                                     </div>
                                 </div>
                             </div>
                             <div class="flex items-center gap-1">
-                                <button type="submit" class="bg-[#DC2626] hover:bg-red-700 text-white font-bold py-2 px-4 pr-6 rounded focus:outline-none focus:shadow-outline">Update</button>
+                                <button type="submit" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Update</button>
                                 <div class="w-1 h-1 relative">
                                     <div class="absolute w-8 h-8 bg-white rounded-[50%] left-[-16px] top-[-14px] text-center text-gray-400 pt-1">or</div>
                                 </div>
-                                <a href="https://inventory-stg.nexdecade.com/roles" class="bg-gray-200 text-gray-700 font-bold py-2 px-4 pl-6 rounded focus:outline-none focus:shadow-outline">Cancel</a>
+                                <a href="https://inventory-stg.nexdecade.com/roles" class="inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-500 border rounded-lg border-neutral-300 gap-x-2 hover:border-red-600 hover:text-red-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">Cancel</a>
                             </div>
                         </div>
                         <div class="clear-both"></div>
@@ -82,12 +105,7 @@
                 </div>
             </dialog>
         </div>
-        <div id="footer" class="w-full text-center text-sm md:text-base py-3">
-            <span class="text-red-700">Copyright ©</span>
-            <span class="text-red-700">2024</span>
-            <span class="font-medium text-teal-700">Nex Inventory.</span>
-            <span class="text-red-700">All Rights Reserved</span>
-        </div>
+
     </div>
 </div>
 
--- resources/views/roles/permission.blade.php
+++ resources/views/roles/permission.blade.php
@@ -1,16 +1,44 @@
 @extends('layouts.app')
-@section('title', 'Users')
+@section('title', 'User Permissions')
+
+@section('pageTitle', 'User Permissions')
+@section('breadcumb')
+    <ol class="flex items-center mt-2 whitespace-nowrap mr-2">
+        <li class="inline-flex items-center">
+            <a class="flex items-center text-sm text-gray-500 hover:text-red-600 dark:hover:text-red-600 focus:outline-none focus:text-neutral-600 dark:hover:text-red-600 dark:focus:text-neutral-600 dark:hover:text-red-600"
+               href="#">
+                <svg class="flex-shrink-0 me-3 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                     stroke-linejoin="round">
+                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                </svg>
+                Home
+            </a>
+            <svg class="flex-shrink-0 mx-2 overflow-visible text-gray-400 size-4 dark:text-neutral-600 dark:hover:text-red-600"
+                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m9 18 6-6-6-6"></path>
+            </svg>
+        </li>
+
+        <li class="inline-flex items-center text-sm font-semibold text-red-600 dark:hover:text-red-600 truncate dark:text-red-600 dark:hover:text-red-600"
+            aria-current="page">
+            User Permissions
+        </li>
+    </ol>
+@endsection
+
 @section('content')
 
-<div class="w-full border rounded-md p-4">
+<div class="flex flex-col p-4 border rounded-lg dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700">
     <h1 class="pb-2">
-        <span class="text-lg font-medium">Permission for the role of</span>
-        <b class="text-xl italic uppercase">Super Admin:</b>
+        <span class="text-lg font-medium text-gray-800 dark:text-white">Permission for the role of</span>
+        <b class="text-xl italic uppercase text-red-600 dark:text-red-600">Super Admin:</b>
     </h1>
-    <hr>
-    <div class="w-full py-4 gap-3 divide-x divide-gray-300">
+    <div class="w-full py-4 gap-3 divide-x divide-neutral-200 dark:divide-neutral-700 border border-neutral-200 dark:border-neutral-700 rounded-md">
        @foreach($categoriWisePermission as $key => $permission)
-                <button type="button" onclick="scrollToSection('section{{$key}}')" class="px-2 py-0 text-sm font-medium">{{ ucfirst($key) }}</button>
+                <button type="button" onclick="scrollToSection('section{{$key}}')" class="px-2 py-0 text-sm font-medium text-gray-800 dark:text-white hover:text-red-600 dark:hover:text-red-600">{{ ucfirst($key) }}</button>
         @endforeach
 
         <!-- <button type="button" onclick="scrollToSection('sectionusers')" class="px-2 py-0 text-sm font-medium">Super Admin</button>
@@ -19,9 +47,9 @@
         <button type="button" onclick="scrollToSection('sectioncategories')" class="px-2 py-0 text-sm font-medium">Team Lead</button>
         <button type="button" onclick="scrollToSection('sectionsuppliers')" class="px-2 py-0 text-sm font-medium">User</button> -->
     </div>
-    <form action="{{ route('permission.update', $role->id)}}" method="POST" enctype="multipart/form-data"> 
-        @csrf 
-      <div class="grid md:grid-cols-2 rounded-[5px] gap-x-10 gap-y-4">
+    <form action="{{ route('permission.update', $role->id)}}" method="POST" enctype="multipart/form-data">
+        @csrf
+      <div class="grid  rounded-[5px] gap-x-10 gap-y-4">
         @foreach($categoriWisePermission as $key=>$permission)
                 @php
                     $i = 0;
@@ -33,22 +61,22 @@
                     }
                 @endphp
         <div class="py-4" id="section{{ $key}}">
-          <div class="rounded-[5px] shadow-lg shadow-slate-100 bg-slate-50 shadow-slate-400 border border-1 border-t-[3px] border-t-[#DC2626] p-5 pt-3">
+          <div class="flex flex-col p-4 border rounded-lg dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700">
             <div class="flex justify-between items-center">
-              <h3 class="text-xl font-semibold">Manage {{ $key}} <span class="text-medium text-gray-500 text-sm">(permission)</span>
+              <h3 class="text-xl font-semibold text-gray-800 dark:text-white">Manage {{ $key}} <span class="text-medium text-gray-500 text-sm">(permission)</span>
               </h3>
-              <label class="cursor-pointer label flex justify-between items-center gap-y-6 rounded-md hover:bg-slate-200">
-                <span class="label-text">Select All</span>
-                   <input type="checkbox" name="all" value="0" class="checkbox checkbox-sm checkbox-info ml-2 selectAllCheckbox" data-section="{{ $key }}" {{$i==$n ? 'checked' : ''}}>
+              <label class="p-2 cursor-pointer label flex justify-between items-center gap-y-6 rounded-md hover:bg-red-600 dark:hover:bg-red-600 hover:text-white">
+                <span class="label-text text-gray-800 hover:text-white dark:text-white ">Select All</span>
+                   <input type="checkbox" name="all" value="0" class="shrink-0 mt-0.5 border-gray-200 rounded text-teal-600 focus:ring-teal-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-teal-500 dark:checked:border-teal-500 dark:focus:ring-offset-teal-800 checkbox checkbox-sm checkbox-info ml-2 selectAllCheckbox" data-section="{{ $key }}" {{$i==$n ? 'checked' : ''}}>
               </label>
             </div>
             <hr class="my-2">
               @foreach($permission as $item)
               <div class="grid grid-cols-1 md:grid-cols-3 gap-x-6 gap-y-6 ">
                   <div class="col-span-3">
-                      <label class="cursor-pointer label flex justify-between items-center gap-y-6 rounded-md hover:bg-slate-200">
-                          <span class="label-text">{{$item->label}}</span>
-                          <input type="checkbox" name="permissionId[]" value="{{ $item->id }}" class="checkbox checkbox-sm checkbox-info ml-2 permissionCheckbox" {{ in_array($item->id, $existingPermissionsIds) ? 'checked' : '' }}>
+                      <label class="p-2 cursor-pointer label flex justify-between items-center gap-y-6 rounded-md hover:bg-red-600 hover:text-white">
+                          <span class="label-text text-gray-800 hover:text-white dark:text-white">{{$item->label}}</span>
+                          <input type="checkbox" name="permissionId[]" value="{{ $item->id }}" class="shrink-0 mt-0.5 border-gray-200 rounded text-teal-600 focus:ring-teal-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-teal-500 dark:checked:border-teal-500 dark:focus:ring-offset-teal-800 checkbox checkbox-sm checkbox-info ml-2 permissionCheckbox" {{ in_array($item->id, $existingPermissionsIds) ? 'checked' : '' }}>
                       </label>
                   </div>
               </div>
@@ -58,32 +86,28 @@
         </div>
 
          @endforeach
-        
+
         <div class="col-span-3 flex justify-end items-center gap-2 relative mt-8 mb-0">
             <div class="flex justify-center items-center mb-3 rounded-md transition duration-150 ease-in-out relative" role="group">
-                <button type="submit" class="inline-flex justify-end items-center gap-x-3 rounded-l bg-[#029397] hover:bg-[#173344] text-white font-medium leading-normal  transition duration-150 ease-in-out focus:bg-[#173344] focus:outline-none focus:ring-0 active:bg-[#223846e7] mr-[-8px] py-2 px-6" data-te-ripple-init data-te-ripple-color="light">
-                        <span class="text-slate-200">
-                            <i class="fa-solid fa-floppy-disk"></i>
-                        </span>
-                    <span>
+                <button type="submit" class="inline-flex items-center text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 mr-[-8px] py-2 px-6" data-te-ripple-init data-te-ripple-color="light">
                             Update
-                        </span>
+
 
                 </button>
                 <div class="inline-flex z-10 bg-gray-200 p-2 text-sm w-8 h-8 rounded-[50%] items-center justify-center leading-normal text-slate-900  transition duration-150 ease-in-out hover:bg-gray-300 focus:outline-none focus:ring-0 " data-te-ripple-init data-te-ripple-color="light">
                     Or
                 </div>
-                <button type="reset" class="inline-flex rounded-r bg-[#36596ee7] hover:bg-[#36596ee7] text-white focus:text-slate-100 active:text-slate-100  font-medium leading-normal  transition duration-150 ease-in-out focus:bg-[#36596ee7] focus:outline-none focus:ring-0 active:bg-[#2d4b5ee7] ml-[-8px] py-2 px-6" data-te-ripple-init data-te-ripple-color="light">
+                <button type="reset" class="inline-flex items-center text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 ml-[-8px] py-2 px-6" data-te-ripple-init data-te-ripple-color="light">
                     Reset
                 </button>
 
-                <a href="{{url('/roles')}}" class="inline-flex ml-4 rounded bg-red-600 hover:bg-red-700 text-white font-medium leading-normal  transition duration-200 ease-in-out py-2 px-6">
+                <a href="{{url('/roles')}}" class="ml-4 inline-flex items-center text-sm font-semibold text-gray-500 border rounded-lg border-neutral-300 gap-x-2 hover:border-red-600 hover:text-red-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 py-2 px-6">
                     Cancel
                 </a>
             </div>
         </div>
 
-        
+
     </form>
   </div>
 <script>

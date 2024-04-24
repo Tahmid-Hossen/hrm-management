e89455e93d050967af3d37ea622f437f112359af
Tahmid Hossen <tahmid.hossen@nexdecade.com>
2024-04-24 13:35:11 +0600

    added the new design in create modal

--- app/Http/Controllers/EmployeeProfileController.php
+++ app/Http/Controllers/EmployeeProfileController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Employee;
+// use App\Models\Employee;
 
 class EmployeeProfileController extends Controller
 {
-    public function index()
-    {
-        // return view('employee-profile.index');
-        $employees = Employee::all();
-        // return $employees;
-        return view('employee-profile.index', compact('employees')); 
-    }
+    // public function index()
+    // {
+    //     // return view('employee-profile.index');
+    //     $employees = Employee::all();
+    //     // return $employees;
+    //     return view('employee-profile.index', compact('employees')); 
+    // }
 }
